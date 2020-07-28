11a97589fc56ab9f78e3902d724817797b2aeec8
abun880007 <abun880007@msn.com>
2020-07-29 00:20:28 +0100

    add Sec Patch levels

--- 420rom.mk
+++ 420rom.mk
@@ -5,3 +5,11 @@ $(call inherit-product, vendor/420rom/config/common_full_phone.mk)
 $(call inherit-product, device/420rom/sepolicy/common/sepolicy.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1440
+
+# Vendor security patch level
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.lineage.build.vendor_security_patch=2020-07-05
+	
+# Security patch level
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.version.security_patch=2020-07-05
