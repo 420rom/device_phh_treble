92c486bfd0cb0668a106290d9e2729cb66603c1d
abun880007 <abun880007@msn.com>
2020-08-09 09:35:52 +0100

    more 420rom
    # Conflicts:
    #	420rom_arm64_aN.mk
    #	420rom_arm64_aS.mk
    #	420rom_arm64_afN.mk
    #	420rom_arm64_afS.mk
    #	420rom_arm64_agN.mk
    #	420rom_arm64_agS.mk
    #	420rom_arm64_avN.mk
    #	420rom_arm64_avS.mk

--- 420rom.mk
+++ 420rom.mk
@@ -1,8 +1,8 @@
 TARGET_GAPPS_ARCH := arm64
 
-$(call inherit-product, vendor/420rom/config/common.mk)
 $(call inherit-product, vendor/420rom/config/common_full_phone.mk)
-$(call inherit-product, device/420rom/sepolicy/common/sepolicy.mk)
+-include vendor/420rom/build/core/config.mk
+-include vendor/420rom/build/core/apicheck.mk
 
 TARGET_BOOT_ANIMATION_RES := 1440
 
