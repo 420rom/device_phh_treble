2c20b7d71962c52afaf294fd330bf99000436e53
abun880007 <abun880007@msn.com>
2020-10-18 19:44:31 +0100

    420rom time

---
+++ 420rom.mk
@@ -0,0 +1,15 @@
+TARGET_GAPPS_ARCH := arm64
+
+$(call inherit-product, vendor/420rom/config/common_full_phone.mk)
+-include vendor/420rom/build/core/config.mk
+-include vendor/420rom/build/core/apicheck.mk
+
+TARGET_BOOT_ANIMATION_RES := 1440
+
+# Vendor security patch level
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.lineage.build.vendor_security_patch=2020-10-05
+	
+# Security patch level
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.version.security_patch=2020-010-05
