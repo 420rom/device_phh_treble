064a556df3288a34dd48f296fa06307e0d29a53c
abun880007 <abun880007@msn.com>
2020-10-27 14:44:36 +0000

    fix build

---
+++ AndroidProducts.mk
@@ -0,0 +1,10 @@
+# Copyright (C) 2018 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/420rom.mk
+
+COMMON_LUNCH_CHOICES := \
+    treble_arm64_bvN-eng \
+    treble_arm64_bvN-user \
+    treble_arm64_bvN-userdebug
